5228f6ed9601d2bc0dca02544a1186d3d905634f
jesus <jesus_adrian_26@hotmail.com>
2021-01-01 23:48:12 -0400

    first deploy

---
+++ src/components/Resumen.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types'
+import {primerMayuscula} from '../helper';
+
+const ContenedoResumen = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838f;
+  color: #fff;
+  margin-top: 1rem;
+`;
+
+const Resumen = ({ datos }) => {
+  //extraer de datos
+
+  const { marca, year, plan } = datos;
+
+  if (marca === "" || year === "" || plan === "") return null;
+
+  return (
+    <Fragment>
+      <ContenedoResumen>
+        <h2>Resumen de Cotizacion</h2>
+        <ul>
+          <li> Marca: { primerMayuscula(marca) } </li>
+          <li> Plan: {primerMayuscula(plan) } </li>
+          <li> Año del Auto: { primerMayuscula(year) } </li>
+        </ul>
+      </ContenedoResumen>
+    </Fragment>
+  );
+};
+Resumen.propTypes={
+datos: PropTypes.object.isRequired
+
+}
+
+export default Resumen;
